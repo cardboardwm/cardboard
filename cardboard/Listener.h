3808f60be27b67a06d3b7a59f67058cd565c6627
alexge50 <alexge50.x@gmail.com>
2020-02-03 10:20:45 +0200

    added ListenerData variant and ListenerList

--- cardboard/Listener.h
+++ cardboard/Listener.h
@@ -2,13 +2,31 @@
 #define CARDBOARD_LISTENER_H
 
 #include <wayland-server.h>
-#include "Server.h"
+#include "View.h"
+
+#include <variant>
+#include <algorithm>
+
+struct NoneT {};
+struct Server;
+
+using ListenerData = std::variant<
+        NoneT,
+        wlr_output*,
+        View*
+    >;
 
 struct Listener
 {
-    wl_listener* listener;
+    wl_listener listener;
     Server* server;
-    void* user_pointer;
+    ListenerData listener_data;
+
+    Listener(wl_notify_func_t notify, Server* server, ListenerData listener_data):
+        listener {{}, notify},
+        server{server},
+        listener_data{ std::move(listener_data) }
+    {}
 };
 
 Server* get_server(wl_listener* listener)
@@ -17,10 +35,40 @@ Server* get_server(wl_listener* listener)
     return l->server;
 }
 
-void* get_user_pointer(wl_listener* listener)
+template <typename T>
+T& get_listener_data(wl_listener* listener)
 {
     Listener* l = wl_container_of(listener, l, listener);
-    return l->user_pointer;
+    return std::get<T>(l->listener_data);
 }
+
+class ListenerList
+{
+public:
+
+    ~ListenerList()
+    {
+        for(auto& listener: listeners)
+            wl_list_remove(&listener.listener.link);
+    }
+
+    void add_listener(wl_signal* signal, Listener&& listener)
+    {
+        listeners.push_back(std::move(listener));
+        wl_signal_add(signal, &listeners.back().listener);
+    }
+
+    void remove_listener(wl_listener* raw_listener)
+    {
+        wl_list_remove(&raw_listener->link);
+
+        listeners.remove_if([raw_listener](auto& listener) {
+            return raw_listener == &listener.listener;
+        });
+    }
+
+private:
+    std::list<Listener> listeners;
+};
 
 #endif //CARDBOARD_LISTENER_H
