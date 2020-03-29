4c7f489c9625b453eb6b4b44636f251ded8f764c
alexge50 <alexge50.x@gmail.com>
2020-03-30 00:33:24 +0300

    added exec and bind commands implementation

--- cardboard/ipc_handlers/commands.h
+++ cardboard/ipc_handlers/commands.h
@@ -28,42 +28,33 @@ inline CommandResult quit(Server* server)
     return { "" };
 }
 
-[[maybe_unused]] static std::array<std::pair<std::string_view, uint32_t>, 8> mod_table = { { { "shift", WLR_MODIFIER_SHIFT },
-                                                                            { "ctrl", WLR_MODIFIER_CTRL },
-                                                                            { "alt", WLR_MODIFIER_ALT },
-                                                                            { "super", WLR_MODIFIER_LOGO },
-                                                                            { "caps", WLR_MODIFIER_CAPS },
-                                                                            { "mod2", WLR_MODIFIER_MOD2 },
-                                                                            { "mod3", WLR_MODIFIER_MOD3 },
-                                                                            { "mod5", WLR_MODIFIER_MOD5 } } };
+[[maybe_unused]] static std::array<std::pair<std::string_view, uint32_t>, 8> mod_table = {  };
 
 
-inline CommandResult bind(Server* /*server*/, std::vector<std::string> /*modifiers*/, std::string /*key*/, Command /*command*/)
+inline CommandResult bind(Server* server, uint32_t modifiers, xkb_keysym_t sym, const Command& command)
 {
-    /*
-    auto command = static_cast<IPCParsedCommand>(std::vector(std::next(cmd.begin(), 2), cmd.end()));
-    auto handler = ipc_find_command_handler(command[0]);
-    if (!handler) {
-        return { "Invalid command.\n" };
-    }
-    server->keybindings_config.map[modifier].insert({ sym, { *handler, command } });
-*/
+    server->keybindings_config.map[modifiers].insert({sym, command});
     return { "" };
 }
 
-inline CommandResult exec(Server* /*server*/, std::vector<std::string> /*command*/)
+inline CommandResult exec(Server*, std::vector<std::string> arguments)
 {
-    /*
-         std::vector<char*> argv;
-    argv.reserve(cmd.size() - 1 + 1);
-    std::transform(std::next(cmd.begin()), cmd.end(), std::back_inserter(argv), [](auto& s) { return const_cast<char*>(s.c_str()); });
+    std::vector<char*> argv;
+    argv.reserve(arguments.size() - 1 + 1);
+    std::transform(
+        std::next(arguments.begin()),
+        arguments.end(),
+        std::back_inserter(argv),
+        [](auto& s) { return const_cast<char*>(s.c_str());
+    });
     argv.push_back(nullptr);
 
     if (fork() == 0) {
         setsid();
         execvpe(argv[0], argv.data(), environ);
         exit(0);
-    }*/
+    }
+
     return { "" };
 }
 
--- cardboard/ipc_handlers/dispatch_command.cpp
+++ cardboard/ipc_handlers/dispatch_command.cpp
@@ -6,6 +6,7 @@
 #include "../Command.h"
 
 #include <variant>
+#include <numeric>
 
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
@@ -26,11 +27,31 @@ Command dispatch_command(const CommandData& command_data)
             return commands::quit;
         },
         [](const CommandArguments::bind& bind_data) -> Command {
-            std::string key_binding;
-            Command command = dispatch_command(*(bind_data.command));
-            return [key_binding, command](Server* server) {
-                return commands::bind(server, {}, {}, command);
+            static const std::unordered_map<std::string, uint32_t> mod_table = {
+                  { "shift", WLR_MODIFIER_SHIFT },
+                  { "ctrl", WLR_MODIFIER_CTRL },
+                  { "alt", WLR_MODIFIER_ALT },
+                  { "super", WLR_MODIFIER_LOGO },
+                  { "caps", WLR_MODIFIER_CAPS },
+                  { "mod2", WLR_MODIFIER_MOD2 },
+                  { "mod3", WLR_MODIFIER_MOD3 },
+                  { "mod5", WLR_MODIFIER_MOD5 }
             };
+
+            uint32_t modifiers = 0;
+            for(auto mod: bind_data.modifiers)
+                modifiers |= mod_table.at(mod);
+
+            xkb_keysym_t sym = xkb_keysym_from_name(bind_data.key.c_str(), XKB_KEYSYM_CASE_INSENSITIVE);
+
+            if(sym == XKB_KEY_NoSymbol)
+                return {[bind_data](Server*) -> CommandResult
+                    { return {std::string("Invalid keysym: ") + bind_data.key}; }};
+
+            Command command = dispatch_command(*(bind_data.command));
+            return {[modifiers, sym, command](Server* server) {
+                return commands::bind(server, modifiers, sym, command);
+            }};
         },
         [](CommandArguments::exec exec_data) -> Command {
             return [exec_data](Server* server) {
