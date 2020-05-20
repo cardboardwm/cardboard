b2e25345d0044169d2581f136cac4bcea53554db
Tudor Roman <tudor@tudorr.ro>
2020-05-20 11:45:52 +0300

    moved all handlers from EventHandlers to Server (temporary)

--- cardboard/Server.cpp
+++ cardboard/Server.cpp
@@ -79,16 +79,16 @@ bool Server::init()
         wl_signal* signal;
         wl_notify_func_t notify;
     } to_add_listeners[] = {
-        { &backend->events.new_input, new_input_handler },
-        { &backend->events.new_output, new_output_handler },
+        { &backend->events.new_input, Server::new_input_handler },
+        { &backend->events.new_output, Server::new_output_handler },
 
-        { &output_layout->events.add, output_layout_add_handler },
+        { &output_layout->events.add, Server::output_layout_add_handler },
 
-        { &xdg_shell->events.new_surface, new_xdg_surface_handler },
-        { &layer_shell->events.new_surface, new_layer_surface_handler },
+        { &xdg_shell->events.new_surface, Server::new_xdg_surface_handler },
+        { &layer_shell->events.new_surface, Server::new_layer_surface_handler },
 
-        { &inhibit_manager->events.activate, activate_inhibit_handler },
-        { &inhibit_manager->events.deactivate, deactivate_inhibit_handler },
+        { &inhibit_manager->events.activate, Server::activate_inhibit_handler },
+        { &inhibit_manager->events.deactivate, Server::deactivate_inhibit_handler },
     };
 
     for (const auto& to_add_listener : to_add_listeners) {
@@ -339,4 +339,147 @@ void Server::teardown(int code)
 {
     wl_display_terminate(wl_display);
     exit_code = code;
+}
+
+void Server::new_output_handler(struct wl_listener* listener, void* data)
+{
+    Server* server = get_server(listener);
+    auto* output = static_cast<struct wlr_output*>(data);
+
+    // pick the monitor's preferred mode
+    if (!wl_list_empty(&output->modes)) {
+        struct wlr_output_mode* mode = wlr_output_preferred_mode(output);
+        wlr_output_set_mode(output, mode);
+        wlr_output_enable(output, true);
+        if (!wlr_output_commit(output)) {
+            return;
+        }
+    }
+
+    // add output to the layout. add_auto arranges outputs left-to-right
+    // in the order they appear.
+    wlr_output_layout_add_auto(server->output_layout, output);
+}
+
+void Server::output_layout_add_handler(struct wl_listener* listener, void* data)
+{
+    Server* server = get_server(listener);
+    auto* l_output = static_cast<struct wlr_output_layout_output*>(data);
+
+    auto output_ = Output {};
+    output_.wlr_output = l_output->output;
+    wlr_output_effective_resolution(output_.wlr_output, &output_.usable_area.width, &output_.usable_area.height);
+    register_output(server, std::move(output_));
+
+    auto& output = server->outputs.back();
+
+    Workspace* ws_to_assign = nullptr;
+    for (auto& ws : server->workspaces) {
+        if (!ws.output) {
+            ws_to_assign = &ws;
+            break;
+        }
+    }
+
+    if (!ws_to_assign) {
+        ws_to_assign = &server->create_workspace();
+    }
+
+    ws_to_assign->activate(output);
+
+    // the output doesn't need to be exposed as a wayland global
+    // because wlr_output_layout does it for us already
+}
+
+void Server::new_xdg_surface_handler(struct wl_listener* listener, void* data)
+{
+    Server* server = get_server(listener);
+    auto* xdg_surface = static_cast<struct wlr_xdg_surface*>(data);
+
+    if (xdg_surface->role != WLR_XDG_SURFACE_ROLE_TOPLEVEL) {
+        return;
+    }
+
+    create_view(server, new XDGView(xdg_surface));
+}
+
+void Server::new_layer_surface_handler(struct wl_listener* listener, void* data)
+{
+    Server* server = get_server(listener);
+    auto* layer_surface = static_cast<struct wlr_layer_surface_v1*>(data);
+    wlr_log(WLR_DEBUG,
+            "new layer surface: namespace %s layer %d anchor %d "
+            "size %dx%d margin %d,%d,%d,%d",
+            layer_surface->namespace_,
+            layer_surface->client_pending.layer,
+            layer_surface->client_pending.anchor,
+            layer_surface->client_pending.desired_width,
+            layer_surface->client_pending.desired_height,
+            layer_surface->client_pending.margin.top,
+            layer_surface->client_pending.margin.right,
+            layer_surface->client_pending.margin.bottom,
+            layer_surface->client_pending.margin.left);
+
+    OptionalRef<Output> output_to_assign;
+    if (layer_surface->output) {
+        output_to_assign = OptionalRef(static_cast<Output*>(layer_surface->output->data));
+    } else {
+        // Assigns output of the focused workspace
+        output_to_assign = server->seat.get_focused_workspace(server)
+                               .and_then<Output>([](auto& ws) { return ws.output; })
+                               .or_else([server]() {
+                                   if (server->outputs.empty()) {
+                                       return OptionalRef<Output>(nullptr);
+                                   }
+                                   return OptionalRef(server->outputs.front());
+                               })
+                               .or_else([layer_surface]() { wlr_layer_surface_v1_close(layer_surface); return NullRef<Output>; });
+    }
+
+    output_to_assign.and_then([output_to_assign, layer_surface, server](auto& output) {
+        LayerSurface ls;
+        ls.output = output_to_assign;
+        ls.surface = layer_surface;
+        ls.surface->output = output.wlr_output;
+        create_layer(server, std::move(ls));
+    });
+}
+
+#if HAVE_XWAYLAND
+void Server::new_xwayland_surface_handler(struct wl_listener* listener, void* data)
+{
+    auto* server = get_server(listener);
+    auto* xsurface = static_cast<struct wlr_xwayland_surface*>(data);
+
+    if (xsurface->override_redirect) {
+        create_xwayland_or_surface(server, xsurface);
+        return;
+    }
+
+    wlr_log(WLR_DEBUG, "new xwayland surface title='%s' class='%s'", xsurface->title, xsurface->class_);
+    create_view(server, new XwaylandView(server, xsurface));
+}
+#endif
+
+void Server::new_input_handler(struct wl_listener* listener, void* data)
+{
+    Server* server = get_server(listener);
+
+    auto* device = static_cast<struct wlr_input_device*>(data);
+
+    server->seat.add_input_device(server, device);
+}
+
+void Server::activate_inhibit_handler(struct wl_listener* listener, void*)
+{
+    auto* server = get_server(listener);
+
+    server->seat.set_exclusive_client(server, server->inhibit_manager->active_client);
+}
+
+void Server::deactivate_inhibit_handler(struct wl_listener* listener, void*)
+{
+    auto* server = get_server(listener);
+
+    server->seat.set_exclusive_client(server, nullptr);
 }
--- cardboard/Server.h
+++ cardboard/Server.h
@@ -29,7 +29,6 @@ extern "C" {
 #include <vector>
 
 #include "Config.h"
-#include "EventListeners.h"
 #include "IPC.h"
 #include "Keyboard.h"
 #include "Layers.h"
@@ -137,6 +136,70 @@ struct Server {
     void stop();
     /// Stops the event loop. Runs before Server::stop.
     void teardown(int code);
+
+private:
+    /**
+    * \brief Executed when a new output (monitor) is attached.
+    *
+    * It adds the output to Server::output_layout. The output
+    * will be processed further after Server::output_layout signals
+    * that the output has been added and configured.
+    *
+    * \sa output_layout_add_handler Called after Server::output_layout adds and configures the output.
+    */
+    static void new_output_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Processes the output after it has been configured by Server::output_layout.
+    *
+    * After that, the compositor stores it and registers some event handlers.
+    * The compositor then assigns a workspace to this output, creating one if none is available.
+    */
+    static void output_layout_add_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Called when a new \c xdg_surface is created by a client.
+    *
+    * An \c xdg-surface is a type of surface exposed by the \c xdg-shell.
+    * This handler creates a View based on it.
+    */
+    static void new_xdg_surface_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Called when a new \c layer_surface is created by a client.
+    *
+    * A \c layer_surface is a type of surface exposed by the \c layer-shell.
+    * This handler does NOT create Views based on it.
+    */
+    static void new_layer_surface_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Called when a new \c xwayland_surface is spawned by an X client.
+    *
+    * An \c xwayland_surface is a type of surface exposed by the xwayland wlroots system.
+    */
+    static void new_xwayland_surface_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Called when an input device (keyboard, mouse, touchscreen, tablet) is attached.
+    *
+    * The device is registered within the compositor accordingly.
+    */
+    static void new_input_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Activates the inhibition of input events for the requester.
+    *
+    * Per the wlr-input-inhibitor protocol.
+    */
+    static void activate_inhibit_handler(struct wl_listener* listener, void* data);
+
+    /**
+    * \brief Deactivates the inhibiton of input events for the requester.
+    *
+    * Per the wlr-input-inhibitor protocol.
+    */
+    static void deactivate_inhibit_handler(struct wl_listener* listener, void* data);
 };
 
 #endif // __CARDBOARD_SERVER_H_
