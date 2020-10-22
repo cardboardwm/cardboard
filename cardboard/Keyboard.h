636df897dacd177bfd8a223192032cf8648e87b7
Tudor Roman <tudor@tudorr.ro>
2020-10-22 17:33:11 +0300

    added license

--- cardboard/Keyboard.h
+++ cardboard/Keyboard.h
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+Copyright (C) 2020 Alexandru-Iulian Magan, Tudor-Ioan Roman, and contributors.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef CARDBOARD_KEYBOARD_H_INCLUDED
 #define CARDBOARD_KEYBOARD_H_INCLUDED
 
